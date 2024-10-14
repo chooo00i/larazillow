21584bf53dc6960643c45138e22a1ffe233efcdf
chooo00i <dkfks961025@gmail.com>
2024-10-14 23:13:30 +0900

    first_commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <Link href="/">Main page</Link>&nbsp;
+    <Link href="/hello">Show page</Link>
+    <div>The page with time {{ timer }}</div>
+    <slot>Default</slot>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+    import {ref} from 'vue'
+
+    let timer = ref(0)
+    setInterval(() => timer.value++, 1000);
+</script>
