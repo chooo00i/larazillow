890aad38052bfe3e2d52962809ea66f682746ce5
chooo00i <dkfks961025@gmail.com>
2024-10-15 23:53:10 +0900

    add Listing vue

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- v-for의 iteration에는 고유 key가 필요함 -->
+    <!-- <div v-for="(listing, index) in listings" :key="listing.id">  -->
+    <div v-for="listing in listings" :key="listing.id"> 
+        <Link :href="`/listing/${listing.id}`">
+            <ListingAdress :listing="listing"/>
+        </Link>
+    </div>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+    import ListingAdress from '@/Components/ListingAdress.vue';
+    defineProps({
+        listings: Array,
+    })
+</script>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ListingAdress :listing="listing"/>
+    </div>
+</template>
+
+<script setup>
+    import ListingAdress from '@/Components/ListingAdress.vue';
+    defineProps({
+        listing: Object,
+    })
+</script>
