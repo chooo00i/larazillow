4c1a9e00cb58d039f50e19be0d12ce50112005a5
chooo00i <dkfks961025@gmail.com>
2024-10-16 22:40:22 +0900

    .gitignore update

---
+++ resources/js/Pages/Listing/Create.vue
@@ -0,0 +1,60 @@
+<template>
+    <form>
+        <div>
+            <div>
+                <label>Beds</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Baths</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Area</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>City</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Post Code</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Street</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Street Nr</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Price</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <button type="submit">Create</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<!-- scoped 해당 컴포넌트에만 스타일이 적용되도록 함 -->
+<style scoped>
+    label {
+    margin-right: 2em;
+    }
+
+    div {
+    padding: 2px
+    }
+</style>
