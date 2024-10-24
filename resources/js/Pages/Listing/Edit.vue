e083cca0e9917fc1eabccbd945c4e3e54f8d87a7
chooo00i <dkfks961025@gmail.com>
2024-10-25 00:13:15 +0900

    add style to error message

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -1,69 +1,69 @@
 <template>
     <form @submit.prevent="update()">
-        <div>
-            <div>
-                <label>Beds</label>
-                <input v-model.number="form.beds" type="text" />
+        <div class="grid grid-cols-6 gap-4">
+            <div class="col-span-2">
+                <label class="label">Beds</label>
+                <input class="input" v-model.number="form.beds" type="text" />
             </div>
 
-            <div>
-                <label>Baths</label>
-                <input v-model.number="form.baths" type="text" />
-                <div v-if="form.errors.beds">
+            <div class="col-span-2">
+                <label class="label">Baths</label>
+                <input class="input" v-model.number="form.baths" type="text" />
+                <div v-if="form.errors.beds" class="input-error">
                     {{ form.errors.beds }}
                 </div>
             </div>
 
-            <div>
-                <label>Area</label>
-                <input v-model.number="form.area" type="text" />
-                <div v-if="form.errors.area">
+            <div class="col-span-2">
+                <label class="label">Area</label>
+                <input class="input" v-model.number="form.area" type="text" />
+                <div v-if="form.errors.area" class="input-error">
                     {{ form.errors.area }}
                 </div>
             </div>
 
-            <div>
-                <label>City</label>
-                <input v-model="form.city" type="text" />
-                <div v-if="form.errors.city">
+            <div class="col-span-4">
+                <label class="label">City</label>
+                <input class="input" v-model="form.city" type="text" />
+                <div v-if="form.errors.city" class="input-error">
                     {{ form.errors.city }}
                 </div>
             </div>
 
-            <div>
-                <label>Post Code</label>
-                <input v-model="form.code" type="text" />
-                <div v-if="form.errors.code">
+            <div class="col-span-2">
+                <label class="label">Post Code</label>
+                <input class="input" v-model="form.code" type="text" />
+                <div v-if="form.errors.code" class="input-error">
                     {{ form.errors.code }}
                 </div>
             </div>
 
-            <div>
-                <label>Street</label>
-                <input v-model="form.street" type="text" />
-                <div v-if="form.errors.street">
+            <div class="col-span-4">
+                <label class="label">Street</label>
+                <input class="input" v-model="form.street" type="text" />
+                <div v-if="form.errors.street" class="input-error">
                     {{ form.errors.street }}
                 </div>
             </div>
 
-            <div>
-                <label>Street Nr</label>
-                <input v-model.number="form.street_nr" type="text" />
-                <div v-if="form.errors.street_nr">
+            <div class="col-span-2">
+                <label class="label">Street Nr</label>
+                <input class="input" v-model.number="form.street_nr" type="text" />
+                <div v-if="form.errors.street_nr" class="input-error">
                     {{ form.errors.street_nr }}
                 </div>
             </div>
 
-            <div>
-                <label>Price</label>
-                <input v-model.number="form.price" type="text" />
-                <div v-if="form.errors.price">
+            <div class="col-span-6">
+                <label class="label">Price</label>
+                <input class="input" v-model.number="form.price" type="text" />
+                <div v-if="form.errors.price" class="input-error">
                     {{ form.errors.price }}
                 </div>
             </div>
 
-            <div>
-                <button type="submit">Edit</button>
+            <div class="col-span-6">
+                <button class="btn-primary" type="submit">Edit</button>
             </div>
         </div>
     </form>
