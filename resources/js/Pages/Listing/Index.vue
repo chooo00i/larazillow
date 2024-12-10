7599ffaf388c2684f86c71446de32dc028487881
chooo00i <dkfks961025@gmail.com>
2024-12-10 22:19:15 +0900

    etc

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -7,7 +7,7 @@
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing">
         </Listing>
     </div>
-    <div v-if="listings.data.length" class="w-full flex justify-center mt-8 mb-2"> 
+    <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4"> 
         <Pagination :links="listings.links" />
     </div>
 </template>
