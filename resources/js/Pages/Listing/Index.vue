3d8effac796de8f6004258154b97f42bacd6b546
chooo00i <dkfks961025@gmail.com>
2024-12-10 22:18:27 +0900

    add filter markup

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,6 +2,7 @@
     <!-- iterator는 id가 필요 -->
     <!-- 아래처럼 index를 사용할 수도 있음 -->
     <!-- <div v-for="(listing, index) in listings" :key="listing.id"> -->
+    <Filters />
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing">
         </Listing>
@@ -14,6 +15,7 @@
 <script setup>
 import Pagination from "@/Components/UI/Pagination.vue";
 import Listing from "./Index/Components/Listing.vue"
+import Filters from "./Index/Components/Filters.vue";
 defineProps({
     listings: Object,
 })
