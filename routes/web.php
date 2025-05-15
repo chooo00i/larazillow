6596176aa66376e42552d9ecfef3c74b646a731c
chooo00i <dkfks961025@gmail.com>
2025-05-15 20:10:07 +0900

    git test

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 
-Route::resource('user-account', UserAccountController::class)
-    ->only(['create', 'store']);
+Route::resource('user-account', UserAccountController::class)->only(['create', 'store']);
 
 Route::prefix('realtor')
     ->name('realtor.') // 시작 이름 지정
